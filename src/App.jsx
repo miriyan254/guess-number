b6c02bb8a85df501401fcfb0fc040fd477c20ab2
miriyan254 <miriyan254@gmail.com>
2025-04-05 18:00:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,38 @@ import Info from './components/Info';
 import Button from './components/Button';
 import Input from './components/Input';
 
+function inform(sN, gN){
+  let message;
+  if(gN === null){
+    message = 'Guess the number';
+  }else if(gN === sN){
+    message = 'Yes - ' + sN;
+  }else if(gN < sN){
+    message = 'Too small - ' + sN;
+  }else{
+    message = 'Too big - ' + sN;
+  }
+  return message;
+}
+
 function App(){
   const [secNum, setSecNum] = useState( Math.trunc( Math.random() * 100 ) + 1 );
   const [inp, setInp] = useState('');
-  const [guess, setGuess] = useState(null);
-  const secNumInfo = guess === null ? 'Guess the number' : guess === secNum ? 'Yes' : 'No - ' + secNum;
+  const [guessNum, setGuessNum] = useState(null);
+  const secNumInfo = inform(secNum, guessNum);
 
   function handleChange(e){
     setInp( e.target.value ); 
   }
   
   function handleCheck(){
-    setGuess( Number(inp) );
+    setGuessNum( Number(inp) );
   }
 
   function handleNewGame(){
     setSecNum( Math.trunc( Math.random() * 100 ) + 1 );
     setInp('');
-    setGuess(null);
+    setGuessNum(null);
   }
 
   return (
